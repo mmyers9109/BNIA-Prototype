bf9a080c1478852572f857c09c1dd2631a5e5c8b
Keira <keiraskenandore@gmail.com>
2019-11-05 18:31:02 -0600

    functional changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,3 +4,13 @@ function showMoreInfo(topic) {
 function showLessInfo(topic) {
     document.getElementById(topic).style.top = "-700px";
 }
+
+// selecting topics
+function selectTopic(id) {
+   topic = document.getElementById(id);
+    if (topic.classList.contains("selected")) {
+        topic.classList.remove("selected");
+    } else{
+        topic.classList.add("selected");
+    }
+}
